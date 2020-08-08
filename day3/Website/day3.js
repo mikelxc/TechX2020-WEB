2da88a15c34a06ac84b9a5b0800847d610dcb1ec
FreddieYu66 <freddieyu66@gmail.com>
2020-08-08 11:38:43 +0800

    arr

--- day3/Website/day3.js
+++ day3/Website/day3.js
@@ -25,7 +25,7 @@ arraySum = function(targetArray) {
     }
     console.log(sum);
 }
-arraySum(printNum(1, 10, 1));
+//arraySum(printNum(1, 10, 1));
 
 let plus = (x, y) => {
     return x + y;
@@ -33,6 +33,19 @@ let plus = (x, y) => {
 
 const square = x => x*x;
 
-console.log(plus(2, 2));
+//console.log(plus(2, 2));
 
-console.log(square(6));
+//console.log(square(6));
+
+let arr = [[1, 2], [3], [4, 5]];
+let newArr = function(array) {
+    let newArr = [];
+    for (let E = 0; E < array.length; E++) {
+        let a = array[E];
+        for (let e = 0; e < a.length; e++) {
+            newArr.push(a[e]);
+        }
+    }
+    return newArr;
+}
+console.log(newArr(arr));
