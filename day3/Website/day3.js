074831a07dfc447ccb5d3015b49fc2b80e3a0a14
FreddieYu66 <freddieyu66@gmail.com>
2020-08-08 11:39:34 +0800

    .

--- day3/Website/day3.js
+++ day3/Website/day3.js
@@ -37,7 +37,7 @@ const square = x => x*x;
 
 //console.log(square(6));
 
-let arr = [[1, 2], [3], [4, 5]];
+let arr = [[1, 2], [3], [4, 5], [6, 7, 8], [9]];
 let newArr = function(array) {
     let newArr = [];
     for (let E = 0; E < array.length; E++) {
