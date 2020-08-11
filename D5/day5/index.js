09eca1a4309c2f907606a8d97c3d5b480ac76e5c
FreddieYu66 <freddieyu66@gmail.com>
2020-08-11 13:49:05 +0800

    .

--- D5/day5/index.js
+++ D5/day5/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import State from './state';
 
 function App() {
     return (
         <div className="app">
             <h1>React Hooks</h1>
+            <State />
         </div>
     )
 }
