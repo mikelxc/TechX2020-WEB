a0a7fd01205c2d0bdce5b2fcc0243fc22e91d3d0
FreddieYu66 <freddieyu66@gmail.com>
2020-08-11 15:32:54 +0800

    .

--- D5/Homework/src/index.js
+++ D5/Homework/src/index.js
@@ -6,7 +6,38 @@ import Menu from "./components/Menu";
 import data from "./data/recipes.json";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Menu recipes={data} />, document.getElementById("root"));
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then((response) => {
+    return response.json();
+    })
+    .then((data) => {
+    let i = data;
+    let thumb = i.drinks[0].strDrinkThumb;
+    console.log(i);
+    function drinkList() {
+        return React.createElement(
+        "ul",
+        null,
+        React.createElement("li", null, `Name: ${i.drinks[0].strDrink}`),
+        React.createElement("li", null, `Drink ID: ${i.drinks[0].idDrink}`),
+        React.createElement(
+            "li",
+            null,
+            `It is ${i.drinks[0].strAlcoholic.toLowerCase()}.`
+        ),
+        React.createElement(
+            "li",
+            null,
+            `It is a(n) ${i.drinks[0].strCategory.toLowerCase()}.`
+        )
+        );
+    }
+    document.getElementById("thumbnail").src = thumb;
+    ReactDOM.render(
+        React.createElement(drinkList, null, null),
+        document.getElementById("react-container")
+    );
+    });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
