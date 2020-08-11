74ae4286f3b8e2817329f4b76234a406358c222f
FreddieYu66 <freddieyu66@gmail.com>
2020-08-11 10:42:30 +0800

    .

---
+++ D5/my-app/src/components/Menu.js
@@ -0,0 +1,10 @@
+function Menu() {
+    return(
+        <div>
+            <h1>TechX Web Recipes</h1>
+            <Recipe name="Dingxianlou" />
+        </div>
+    );
+}
+
+export default Menu;
---
+++ D5/my-app/src/components/Recipe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Recipe({name, ingredients, steps}){
+    return(
+        <section>
+            <h1>{name}</h1>
+        </section>
+    );
+}
+
+export default Recipe;
