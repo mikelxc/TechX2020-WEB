731a8c48e3c8941230f3d56e1b185fa64af699df
FreddieYu66 <freddieyu66@gmail.com>
2020-08-12 09:38:01 +0800

    .

--- D5/Homework/src/components/Recipe.js
+++ D5/Homework/src/components/Recipe.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import IngredientsList from "./IngredientsList";
-import Instructions from "./Instructions";
 
 function Recipe({ name, drinks, steps }) {
+  const [isPink, setIsPink] = useState(true);
   return (
-    <section id={name.toLowerCase().replace(/ /g, "-")}>
+    <section id={name.toLowerCase().replace(/ /g, "-")}
+      onClick={() => setIsPink(!isPink)}
+      style={{ background: isPink ? "linear-gradient(to bottom, pink, white)" : "linear-gradient(to bottom, purple, white)" }}
+    >
       <h1 id="title">{name}</h1>
       <IngredientsList list={drinks} />
     </section>
