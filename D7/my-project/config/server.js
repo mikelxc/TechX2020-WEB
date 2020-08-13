95d4149c7a18f67129e60915325a0c857fc5417e
FreddieYu66 <freddieyu66@gmail.com>
2020-08-13 10:33:45 +0800

    .

---
+++ D7/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4c8363360dc0511255b3df0ad14fae43'),
+    },
+  },
+});
