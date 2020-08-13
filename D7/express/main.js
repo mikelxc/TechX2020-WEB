4823a3aac9566f79bbe3cd56c713e3217abb3a35
FreddieYu66 <freddieyu66@gmail.com>
2020-08-13 10:08:43 +0800

    .

--- D7/express/main.js
+++ D7/express/main.js
@@ -1,12 +1,16 @@
 "use strict";
 
-const port = 3000,
+const port = 8000,
 	express = require("express"),
 	app = express();
 app
 	.get("/", (req, res) => {
+		console.log(req.param);
+		console.log(req.body);
+		console.log(req.url);
+		console.log(req.query);
 		res.send("Hello, TechXers");
 	})
 	.listen(port, () => {
-		console.log(`The new Express.js server has started an is listening on port number: ${port}`)
-	})
+		console.log(`The new Express.js server has started and is listening on port number: ${port}`);
+	});
