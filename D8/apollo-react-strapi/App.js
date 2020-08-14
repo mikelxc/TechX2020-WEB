b854513eaf4c78a4edb45c5b7b39f80de539f009
FreddieYu66 <freddieyu66@gmail.com>
2020-08-14 10:05:39 +0800

    .

---
+++ D8/apollo-react-strapi/App.js
@@ -0,0 +1,5 @@
+import react from "react";
+import {render} from "react-dom";
+import ApolloClient, { gql } from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+import ClassItem from './ClassItem';
--- D8/apollo-react-strapi/index.js
+++ D8/apollo-react-strapi/index.js
@@ -1,5 +1,30 @@
-import react from "react";
+import React from "react";
 import {render} from "react-dom";
+import ApolloClient, { gql } from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
 
-const App = ()=> (<h1>React is working</h1>)
-render(<App/>, document.getElementById("root"));
+const client = new ApolloClient({uri: "http://localhost:1337/graphql"});
+const ApolloApp = (AppComponent) => (
+    <ApolloProvider client={client}>
+        <AppComponent/>
+    </ApolloProvider>
+)
+client.query({
+    query: gql`
+        query {
+            classes {
+                id
+                classname
+                ranking
+                classimg {
+                    url
+                }
+            }
+        }
+    `
+})
+.then((results) => console.log(results));
+function App(){
+    <h1>See console for results</h1>;
+};
+render(ApolloApp(App), document.getElementById("root"));
